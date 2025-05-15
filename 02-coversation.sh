c88070d70e1be3869e1f9f3ceb076fc744c8ed7b
vinusha <vinushasaimare@gmail.com>
2025-05-15 18:10:51 +0530

    hello world started

--- 02-coversation.sh
+++ 02-coversation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PERSON1=vinu
+PERSON2=lali
+
 echo "vinu: y how do you do"
 echo "lali: i am fine what abouy you"
 echo "vinnu: what are doimg oghtn now?"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=vinu
+PERSON2=lali
+
+
+echo "$PERSON1: y how do you do"
+echo "$PERSON2: what are doimg oghtn now?"
+echo "$PERSON1: i am fine what abouy you"
+echo "$PERSON2: in job search waht about you?"
