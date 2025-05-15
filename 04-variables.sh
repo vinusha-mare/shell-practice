4886d11a229eba88e9829703045e474f177155e3
vinusha <vinushasaimare@gmail.com>
2025-05-15 18:32:12 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: y how do you do"
+echo "$PERSON2: what are doimg oghtn now?"
+echo "$PERSON1: i am fine what abouy you"
+echo "$PERSON2: in job search waht about you?"
