3809f2f438d9c3c4b293e8a4ce147c198fc80a4a
vinusha <vinushasaimare@gmail.com>
2025-05-21 17:28:36 +0530

    install-commands

---
+++ 10-install-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then 
+    echo ERROR:: please run this in root access"
+else
+    echo "you are running with root access"
+fi
+
+dnf install mysql -y
