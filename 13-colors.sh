ae9f16c551affd8e99f27272b4fe0e5704ed483f
vinusha <vinushasaimare@gmail.com>
2025-05-22 18:17:42 +0530

    colors

---
+++ 13-colors.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $USERID -ne 0 ]
+then 
+    echo "$R ERROR:: $N please run this in root access"
+    exit 1 
+else
+    echo "you are running with root access"
+fi
+}
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "python3  is not installed..going to install"
+    dnf install python3 -y
+    if [ $? -eq 0 ]
+    then 
+        echo "installing python3 $2 is ... $G SUCCESS"
+    else
+         echo "$R failed $2"
+         exit 1
+    fi
+else
+    echo " $G already installed nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed..going to install"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+      echo "already installed nothing to do"
+fi
