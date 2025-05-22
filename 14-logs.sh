0c67907c3bf421e6417beef48c31e0915a180d0c
vinusha <vinushasaimare@gmail.com>
2025-05-22 19:47:11 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$($ echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir  -p $LOGS_FOLDER
