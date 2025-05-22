fb60eb7c17eb506495946667e558f7253313f5c9
vinusha <vinushasaimare@gmail.com>
2025-05-22 20:35:27 +0530

    colors

--- 16-loops.sh
+++ 16-loops.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Root user check
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -8,40 +9,38 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "python" "nginx" "httpd")
 
-mkdir  -p $LOGS_FOLDER
-echo "script started executing at: $(date)" 
-    echo "$R ERROR:: $N please run this in root access" | tee -a $LOG_FILE
+# Create log folder if not exists
+mkdir -p $LOGS_FOLDER
 
-
-VALIDATE(){
-if [ $USERID -ne 0 ]
-then 
-    echo "$R ERROR:: $N please run this in root access" | tee -a $LOG_FILE
-    exit 1 
-else
-    echo "you are running with root access" tee -a $LOG_FILE
-fi
+# Validate root access
+VALIDATE() {
+  if [ $USERID -ne 0 ]; then
+    echo -e "${R}ERROR::${N} please run this script with root access" | tee -a $LOG_FILE
+    exit 1
+  else
+    echo -e "${G}SUCCESS::${N} Running with root access" | tee -a $LOG_FILE
+  fi
 }
 
-for package in $@
+# Call validate at the beginning
+echo -e "${Y}Script started executing at: $(date)${N}" | tee -a $LOG_FILE
+VALIDATE
+
+# Loop through input arguments ($@)
+for package in "$@"
 do
   dnf list installed $package &>>$LOG_FILE
-  if [ $? -ne 0 ]
-    then 
-         echo "$package  is not installed..going to install" tee -a $LOG_FILE
-        dnf install $package -y
-    if [ $? -eq 0 ]
-    then 
-        echo "installing $package $2 is ... $G SUCCESS" tee -a $LOG_FILE
+  if [ $? -ne 0 ]; then
+    echo -e "${Y}$package is not installed... installing now${N}" | tee -a $LOG_FILE
+    dnf install $package -y &>>$LOG_FILE
+    if [ $? -eq 0 ]; then
+      echo -e "${G}SUCCESS:: Installed $package${N}" | tee -a $LOG_FILE
     else
-         echo "$R failed $2"
-         exit 1
+      echo -e "${R}ERROR:: Failed to install $package${N}" | tee -a $LOG_FILE
+      exit 1
     fi
-else
-    echo " $G already installed nothing to do" &>>$LOG_FILE
-fi
-
+  else
+    echo -e "${G}$package is already installed. Nothing to do.${N}" | tee -a $LOG_FILE
+  fi
 done
-
